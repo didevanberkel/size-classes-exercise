d2c746edfd759bda0d2048500e4fbf72b366ecd8
Dide van Berkel <didevanberkel@MacBook-Air-van-Dide.local>
2016-03-04 19:42:34 +0100

    Addes files size classes exercise

---
+++ sizing-it-up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sizing-it-up
+//
+//  Created by Dide van Berkel on 04-03-16.
+//  Copyright © 2016 Gary Grape Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
